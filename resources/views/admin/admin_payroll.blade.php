d4244637c74894e4cc694d3b41894697fa02bb7f
Intmainiostream <joshuaxd050@gmail.com>
2026-04-30 15:15:47 +0800

    FIXED ADMIN PAYROLL

--- resources/views/admin/admin_payroll.blade.php
+++ resources/views/admin/admin_payroll.blade.php
@@ -308,7 +308,7 @@
                         </select>
 
                         @canDo('Payroll', 'create')
-                        <button class="btn-primary " @click="showAddPeriodModal=true">
+                        <button class="btn-primary " @click="showCreatePeriod=true">
                             <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"/>
                             </svg>
